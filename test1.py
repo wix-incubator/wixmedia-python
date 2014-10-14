c57bdac8bb1d7ae77046c9ad0ccf27354eec8fe4
Dmitry Shestak <dmitrys@wix.com>
2014-10-14 12:31:19 +0300

    test update

--- test1.py
+++ test1.py
@@ -1,18 +1,19 @@
 from wixmedia import wixmedia_image
 
-image = wixmedia_image.WixMediaImage('uri')
-
+image = wixmedia_image.WixMediaImage('uri', "dog.png")
+print image
 print image.get_img_tag(width=4, alt="golan")
 print image.get_img_tag()
 
-print image.crop().adjust().filter().get_img_tag()
-
+print image.crop(x=10,y=10, w=120, h=120).adjust().filter().get_img_tag()
+image.reset()
+print image.srz(w=120, h=120).adjust().filter("oil", blur=22).get_img_tag()
 ############
 
-from wixmedia import wixmedia_service
+## from wixmedia import wixmedia_service
 
-service = wixmedia_service.WixMediaService(api_key="my_key", api_secret="my_secret")
+## service = wixmedia_service.WixMediaService(api_key="my_key", api_secret="my_secret")
 
-image = service.upload_file_from_path('/files/images/dog.jpg')
+## image = service.upload_file_from_path('/files/images/dog.jpg')
 
-print image.crop().adjust().filter().get_img_tag()
+## print image.crop().adjust().filter().get_img_tag()
