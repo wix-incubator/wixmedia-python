ae8fa4b380e6f7c6542c9784b7f353e481477c55
Golan Parashi <golanp@wix.com>
2014-10-14 14:27:34 +0300

    refactor

--- test1.py
+++ test1.py
@@ -5,7 +5,7 @@ print image
 print image.get_img_tag(width=4, alt="golan")
 print image.get_img_tag()
 
-print image.crop(x=10,y=10, w=120, h=120).adjust().filter().get_img_tag()
+print image.crop(x=10, y=10, w=120, h=120).adjust().filter().get_img_tag()
 image.reset()
 print image.srz(w=120, h=120).adjust().filter("oil", blur=22).get_img_tag()
 image.reset()
--- wixmedia/wixmedia_image.py
+++ wixmedia/wixmedia_image.py
@@ -4,23 +4,23 @@ class WixMediaCommandException(Exception):
 
 
 class WixMediaImage(object):
-    COMMAND_NONE = ""
-    COMMAND_SRZ = "srz"
-    COMMAND_SRB = "srb"
+    COMMAND_NONE   = ""
+    COMMAND_SRZ    = "srz"
+    COMMAND_SRB    = "srb"
     COMMAND_CANVAS = "canvas"
-    COMMAND_FILL = "fill"
-    COMMAND_CROP = "crop"
-
+    COMMAND_FILL   = "fill"
+    COMMAND_CROP   = "crop"
 
     def __init__(self, file_uri, org_name):
         self.file_uri = file_uri
         self.org_name = org_name
-        self.reset()
 
+        self.reset()
 
     def srz(self, w, h, q=85, a=1, radius=0.0, amount=1.1, threshold=2.2):
         if self.command != WixMediaImage.COMMAND_NONE:
-            raise WixMediaCommandException("Command already set: %s. Reset image before applying command." % self.command )
+            raise WixMediaCommandException("Command already set: %s. Reset image before applying command." % self.command)
+
         self.command = WixMediaImage.COMMAND_SRZ
         self.command_params = {"w": w, "h": h,
                                "q": q, "a": a,
@@ -31,7 +31,8 @@ class WixMediaImage(object):
 
     def srb(self, w, h, q=85, radius=0.0, amount=1.1, threshold=2.2):
         if self.command != WixMediaImage.COMMAND_NONE:
-            raise WixMediaCommandException("Command already set: %s. Reset image before applying command." % self.command )
+            raise WixMediaCommandException("Command already set: %s. Reset image before applying command." % self.command)
+
         self.command = WixMediaImage.COMMAND_SRB
         self.command_params = {"w": w, "h": h,
                                "q": q,
@@ -42,37 +43,42 @@ class WixMediaImage(object):
 
     def canvas(self, w, h, q=85, a = 1):
         if self.command != WixMediaImage.COMMAND_NONE:
-            raise WixMediaCommandException("Command already set: %s. Reset image before applying command." % self.command )
+            raise WixMediaCommandException("Command already set: %s. Reset image before applying command." % self.command)
+
         self.command = WixMediaImage.COMMAND_CANVAS
         self.command_params = {"w": w, "h": h,
-                               "q": q, "a":a}
+                               "q": q, "a": a}
         return self
 
     def crop(self, x, y, w, h, q=85):
         if self.command != WixMediaImage.COMMAND_NONE:
-            raise WixMediaCommandException("Command already set: %s. Reset image before applying command." % self.command )
+            raise WixMediaCommandException("Command already set: %s. Reset image before applying command." % self.command)
+
         self.command = WixMediaImage.COMMAND_CROP
-        self.command_params = {"x": x, "y":y,
+        self.command_params = {"x": x, "y": y,
                                "w": w, "h": h,
                                "q": q}
         return self
 
     def fill(self, w, h, q=85):
         if self.command != WixMediaImage.COMMAND_NONE:
-            raise WixMediaCommandException("Command already set: %s. Reset image before applying command." % self.command )
+            raise WixMediaCommandException("Command already set: %s. Reset image before applying command." % self.command)
+
         self.command = WixMediaImage.COMMAND_FILL
         self.command_params = {"w": w, "h": h,
                                "q": q}
         return self
 
     def adjust(self, *props, **adjust_props):
-        adjust_props.update({p:True for p in props})
+        adjust_props.update({p: True for p in props})
         self.adjustment_params.update(adjust_props)
+
         return self
 
     def filter(self, *funcs, **filter_funcs):
-        filter_funcs.update({f:True for f in funcs})
+        filter_funcs.update({f: True for f in funcs})
         self.filter_params.update(filter_funcs)
+
         return self
 
     def reset(self):
@@ -85,18 +91,18 @@ class WixMediaImage(object):
         if self.command != WixMediaImage.COMMAND_NONE:
             params = [self.file_uri,
                       self.command,
-                      ",".join(["%s_%s" % (key, val) for key,val in self.command_params.iteritems()])]
+                      ",".join(["%s_%s" % (key, val) for key, val in self.command_params.iteritems()])]
         else:
             params = [self.file_uri]
 
         if self.adjustment_params:
             params.extend(["adjust",
-                           ",".join(["%s_%s" % (key, val) if val!=True else key
+                           ",".join(["%s_%s" % (key, val) if val != True else key
                                       for key,val in self.adjustment_params.iteritems()])
                            ])
         if self.filter_params:
             params.extend(["filter",
-                           ",".join(["%s_%s" % (key, val) if val!=True else key
+                           ",".join(["%s_%s" % (key, val) if val != True else key
                                      for key,val in self.filter_params.iteritems()])
                            ])
         params.append(self.org_name)
