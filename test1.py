01f98c671d6265ff750f958706b244e95010bd4f
Golan Parashi <golanp@wix.com>
2014-10-19 11:48:10 +0300

    sample url changed

--- test1.py
+++ test1.py
@@ -1,8 +1,7 @@
 from wixmedia import wixmedia_image
 
-url   = 'http://media.wixapps.net/goog:234234234234234/ae1d86b24054482f8477bfbf2d426936.png/dog.png'
+url   = 'http://media.wixapps.net/goog-098152434167072483196/images/ae1d86b24054482f8477bfbf2d426936/dog.png'
 image = wixmedia_image.WixMediaImage(url)
-#print image
 
 #print image.crop(x=10, y=10, width=120, height=120).adjust().filter().get_img_tag()
 #image.reset()
